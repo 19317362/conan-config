b2588b85d7dd37c7d597552fbbbbad528e396d17
19317362 <19317362@qq.com>
2019-10-07 16:47:40 +0800

    before test

---
+++ test/bld.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#usage:
+function usage()
+{
+    echo "bld.sh <docker image> <profile>"
+}
+# if [ $# -eq 0 ]
+if [ $# -ne 2 ]
+  then
+    usage
+    exit 1
+fi
+
+docker_img="${1}"
+profile="${2}"
+
+docker run --rm \
+    -v$(pwd):/home/conan/project \
+    -v ~/.conan:/home/conan/.conan \
+    -w /home/conan/project conanio/${docker_img} \
+    /bin/bash -c \
+    "bld_in_docker.sh ${profile}"
