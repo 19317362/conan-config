c279f6ae934d2b7607d7f986cf30d2cd48062cb8
19317362 <19317362@qq.com>
2019-10-07 21:29:54 +0800

    boost build ok

--- test/exec_in_docker.sh
+++ test/exec_in_docker.sh
@@ -7,7 +7,7 @@ unset HTTPS_PROXY
 unset http_proxy
 unset https_proxy
 # conan remote add -i 0 -f  my http://172.29.96.1:8081/artifactory/api/conan/conan False
-
+# rm /home/conan/.conan/data/Poco/1.9.4/pocoproject/stable/package/85083e94b08c445dd831c8e3e6b70508f9ab6a70/lib/libPocoFoundation.a
 # conan profile update settings.compiler.libcxx=libstdc++11 default
 triplet="$1"
 conan profile update settings.compiler.libcxx=libstdc++11 ${triplet}
