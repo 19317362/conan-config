d114b4d8be3ac90f29ef58a5df4d2cd4a31477e0
19317362 <19317362@qq.com>
2019-10-07 20:32:51 +0800

    copy build sh from test to here

---
+++ boost/bld.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#usage:
+function usage()
+{
+    echo "Usage:"
+    echo "./bld.sh <docker image> <profile>"
+    echo "eg."
+    echo "./bld.sh conanio/gcc9-armv7 linux_armv7"
+    echo "./bld.sh conanio/gcc9-armv8 linux_armv8"
+    echo "./bld.sh conanio/android-clang8-armv7 android-clang8-armv7"
+    echo "./bld.sh conanio/android-clang8-armv8 android-clang8-armv8"    
+}
+# if [ $# -eq 0 ]
+if [ $# -ne 2 ]
+  then
+    usage
+    exit 1
+fi
+
+docker_img="${1}"
+profile="${2}"
+
+docker run --rm \
+    -v$(pwd):/home/conan/project \
+    -v ~/.conan:/home/conan/.conan \
+    -w /home/conan/project ${docker_img} \
+    /bin/bash -c \
+    "./exec_in_docker.sh ${profile}"
